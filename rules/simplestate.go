3985cebd2115bfe3eaa7f0db9e0e9367516360b7
James Bell <james@bellstone.ca>
2019-03-26 11:57:57 -0600

    Make SimpleState public

--- rules/simplestate.go
+++ rules/simplestate.go
@@ -2,36 +2,36 @@ package rules
 
 // SimpleState provides a simplified state. It is only valid for 2 players.
 type SimpleState struct {
-	// discards is all of the cards discarded so far (unsorted, unattributed)
-	discards Deck
+	// Discards is all of the cards discarded so far (unsorted, unattributed)
+	Discards Deck
 
-	// highCard is the current player's high card
-	highCard Card
+	// HighCard is the current player's high card
+	HighCard Card
 
-	// lowCard is the current player's low card
-	lowCard Card
+	// LowCard is the current player's low card
+	LowCard Card
 
-	// opponentCard is the card most recently played by the opponent
-	opponentCard Card
+	// OpponentCard is the card most recently played by the opponent
+	OpponentCard Card
 
-	// scoreDiff is the current player's score lead compared to the opponent
-	scoreDiff int
+	// ScoreDiff is the current player's score lead compared to the opponent
+	ScoreDiff int
 }
 
 // SimpleState converts a Gamestate to a SimpleState
 func (state *Gamestate) AsSimpleState() SimpleState {
 	simple := SimpleState{}
 
-	simple.discards = state.Faceup.AsDeck()
+	simple.Discards = state.Faceup.AsDeck()
 	for _, val := range state.PlayerHistory {
-		simple.discards.AddStack(val)
+		simple.Discards.AddStack(val)
 	}
 	if state.ActiveCardIsHighest() {
-		simple.highCard = state.ActivePlayerCard
-		simple.lowCard = state.CardInHand[state.ActivePlayer]
+		simple.HighCard = state.ActivePlayerCard
+		simple.LowCard = state.CardInHand[state.ActivePlayer]
 	} else {
-		simple.lowCard = state.ActivePlayerCard
-		simple.highCard = state.CardInHand[state.ActivePlayer]
+		simple.LowCard = state.ActivePlayerCard
+		simple.HighCard = state.CardInHand[state.ActivePlayer]
 	}
 
 	// Figure out opponent's ID
@@ -41,9 +41,9 @@ func (state *Gamestate) AsSimpleState() SimpleState {
 	}
 	if len(state.PlayerHistory[opponent]) > 0 {
 		// Get opponent's last played card. (Note if the opponent played the Prince, then this will show the discarded card, instead.)
-		simple.opponentCard = state.PlayerHistory[opponent][len(state.PlayerHistory[opponent])-1]
+		simple.OpponentCard = state.PlayerHistory[opponent][len(state.PlayerHistory[opponent])-1]
 	} // else default to None
-	simple.scoreDiff = state.PlayerHistory[state.ActivePlayer].Score() - state.PlayerHistory[opponent].Score()
+	simple.ScoreDiff = state.PlayerHistory[state.ActivePlayer].Score() - state.PlayerHistory[opponent].Score()
 
 	return simple
 }
