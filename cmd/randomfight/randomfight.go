96e32fb369a4ddb158f6bb366fc88497c5ba8272
James Bell <james@bellstone.ca>
2019-03-28 15:53:29 -0600

    Run more random fights

--- cmd/randomfight/randomfight.go
+++ cmd/randomfight/randomfight.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"love-letter-ai/gamemaster"
 	"love-letter-ai/players"
+	"math/rand"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	pls := []players.Player{
 		&players.RandomPlayer{},
 		&players.RandomPlayer{},
@@ -15,7 +18,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	winner, err := gm.PlaySeries(100)
+	winner, err := gm.PlaySeries(1000)
 	if err != nil {
 		panic(err)
 	}
