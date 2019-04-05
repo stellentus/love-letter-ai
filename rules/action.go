4673b76cddaac9053d4cd9617b740f330d4c0c5f
James Bell <james@bellstone.ca>
2019-04-05 10:40:44 -0600

    Implement (Action).AsInt()

--- rules/action.go
+++ rules/action.go
@@ -10,3 +10,18 @@ type Action struct {
 	// SelectedCard is set to the Card chosen by the action, if applicable.
 	SelectedCard Card
 }
+
+// AsInt converts an action to an integer to be used for indexing.
+// This integer only uses 4 bits.
+func (act Action) AsInt() int {
+	retVal := 0
+	if act.PlayRecent {
+		retVal = 1
+	}
+	if act.TargetPlayer != 0 {
+		retVal += 2 * act.TargetPlayer
+	} else if act.SelectedCard != None {
+		retVal += 2 * (int(act.SelectedCard) - 1)
+	}
+	return retVal
+}
