6c397f5bfb000cb41a6e7bbd9b8fa382bf970dce
James Bell <james@bellstone.ca>
2019-04-05 09:48:47 -0600

    Comment TraceOneGame

--- gamemaster/trace.go
+++ gamemaster/trace.go
@@ -7,7 +7,9 @@ import (
 	"love-letter-ai/state"
 )
 
-func TraceOneGame(p players.Player) ([]int, int, error) {
+// TraceOneGame returns the states for one gameplay played by the provided player pl.
+// The return values are a slice of states, the index of the winning player, and an error.
+func TraceOneGame(pl players.Player) ([]int, int, error) {
 	sg, err := rules.NewGame(2)
 	if err != nil {
 		return nil, 0, err
@@ -25,7 +27,7 @@ func TraceOneGame(p players.Player) ([]int, int, error) {
 			return nil, 0, fmt.Errorf("Negative state was calculated: %d", ss)
 		}
 		states = append(states, ss)
-		if err := sg.PlayCard(p.PlayCard(s, sg.ActivePlayer)); err != nil {
+		if err := sg.PlayCard(pl.PlayCard(s, sg.ActivePlayer)); err != nil {
 			fmt.Printf("Game failed: %+v\n", sg)
 			panic(err)
 		}
