290abaf1ccb4e09f17e9b2bc46beb6e0223be750
James Bell <james@bellstone.ca>
2019-04-05 12:47:10 -0600

    Refactor tests to share array

--- rules/action_test.go
+++ rules/action_test.go
@@ -6,21 +6,29 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var tests = []struct {
+	descr  string
+	action Action
+	out    int
+}{
+	// An empty action should be the same as selecting a Guard.
+	{"Empty action", Action{}, 0},
+	{"Target Guard is empty", Action{SelectedCard: Guard}, 0},
+
+	// Test a few different actions
+	{"Maximal action", Action{PlayRecent: true, SelectedCard: Princess}, 15},
+	{"Target player", Action{PlayRecent: true, TargetPlayer: 1}, 3},
+	{"Nearly maximal", Action{PlayRecent: false, SelectedCard: Princess}, 14},
+
+	// When TargetPlayer and SelectedCard are both set, ignore SelectedCard for conversion
+	{"Conflict input", Action{PlayRecent: true, TargetPlayer: 1, SelectedCard: Princess}, 3},
+
+	// Note these two states give the same output; that's okay because these occur when the played card is different, so these actions show up in different contexts.
+	{"Target 1 equals Selected Priest", Action{PlayRecent: false, TargetPlayer: 1}, 2},
+	{"Selected Priest equals Target 1", Action{PlayRecent: false, SelectedCard: Priest}, 2},
+}
+
 func TestHandValues(t *testing.T) {
-	tests := []struct {
-		descr  string
-		action Action
-		out    int
-	}{
-		{"Empty action", Action{}, 0},                             // This should be the same as selecting Guard.
-		{"Target Guard is empty", Action{SelectedCard: Guard}, 0}, // This is the true minimal input, but 'None' must also work
-		{"Maximal action", Action{PlayRecent: true, SelectedCard: Princess}, 15},
-		{"Maximal target", Action{PlayRecent: true, TargetPlayer: 1}, 3},
-		{"Conflict input", Action{PlayRecent: true, TargetPlayer: 1, SelectedCard: Princess}, 3}, // when TargetPlayer and SelectedCard are both set, ignore SelectedCard
-		{"Maximal target", Action{PlayRecent: false, SelectedCard: Princess}, 14},
-		{"Target 1 equals Selected Priest", Action{PlayRecent: false, TargetPlayer: 1}, 2},
-		{"Selected Priest equals Target 1", Action{PlayRecent: false, SelectedCard: Priest}, 2}, // note two states give the same output; that's okay because these occur when the played card is different, so these actions show up in different contexts
-	}
 	for _, test := range tests {
 		assert.EqualValues(t, test.out, test.action.AsInt(), test.descr)
 	}
