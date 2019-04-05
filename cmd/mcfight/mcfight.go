0036a995960b234c23707efe4fe5be462df5bcff
James Bell <james@bellstone.ca>
2019-04-05 16:45:11 -0600

    Make MCFight a bit more interesting with more rounds and output

--- cmd/mcfight/mcfight.go
+++ cmd/mcfight/mcfight.go
@@ -14,10 +14,22 @@ const (
 
 func main() {
 	pl := montecarlo.NewQPlayer(epsilon)
-	fmt.Println("Running...")
-	pl.TrainWithPlayerPolicy(rounds, &players.RandomPlayer{})
 
-	for i := 0; i < 20; i++ {
+	fmt.Println("Running vs random...")
+	pl.TrainWithPlayerPolicy(rounds, &players.RandomPlayer{})
+	printTraces(20, pl)
+	fightRandom(1000, pl)
+
+	for j := 0; j < 5; j++ {
+		fmt.Printf("Running vs self %d...\n", j+1)
+		pl.TrainWithSelfPolicy(rounds)
+		printTraces(20, pl)
+		fightRandom(1000, pl)
+	}
+}
+
+func printTraces(n int, pl *montecarlo.QPlayer) {
+	for i := 0; i < n; i++ {
 		tr, err := gamemaster.TraceOneGame(&players.RandomPlayer{})
 		if err != nil {
 			panic(err.Error())
@@ -27,4 +39,31 @@ func main() {
 			fmt.Printf("    % 8d: %0.3f\n", v.ActionState, pl.Value(v.ActionState))
 		}
 	}
+}
+
+func fightRandom(n int, pl *montecarlo.QPlayer) {
+	fmt.Printf("MC is 0; ")
+	fightPlayers(n, []players.Player{
+		pl,
+		&players.RandomPlayer{},
+	})
+	fmt.Printf("MC is 1; ")
+	fightPlayers(n, []players.Player{
+		&players.RandomPlayer{},
+		pl,
+	})
+}
+
+func fightPlayers(n int, pls []players.Player) {
+	// Now fight vs Random
+	gm, err := gamemaster.New(pls)
+	if err != nil {
+		panic(err)
+	}
+	winner, err := gm.PlaySeries(n)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Player %d won %d-%d\n", winner, gm.Wins[winner], gm.Wins[(winner+1)%2])
 }
