dcb37134e124fdda8d1f3c90055d2995da73c1d5
James Bell <james@bellstone.ca>
2019-04-05 12:22:34 -0600

    Calculate both state indices at once

--- state/action.go
+++ state/action.go
@@ -10,5 +10,15 @@ const (
 
 // ActionIndex returns a unique number between 0 and ActionSpaceMagnitude-1 for the given state.
 func ActionIndex(seenCards rules.Deck, recent, old, opponent rules.Card, scoreDelta int, act rules.Action) int {
-	return (Index(seenCards, recent, old, opponent, scoreDelta) << 4) + act.AsInt()
+	return actionIndex(Index(seenCards, recent, old, opponent, scoreDelta), act)
+}
+
+// Indices returns the ActionIndex and the state Index.
+func Indices(seenCards rules.Deck, recent, old, opponent rules.Card, scoreDelta int, act rules.Action) (int, int) {
+	stateIndex := Index(seenCards, recent, old, opponent, scoreDelta)
+	return actionIndex(stateIndex, act), stateIndex
+}
+
+func actionIndex(stateIndex int, act rules.Action) int {
+	return (stateIndex << 4) + act.AsInt()
 }
