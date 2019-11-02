44b1410564d53a2d7a9108a41d47f012bbcc6570
James Bell <james@stellentus.com>
2019-11-02 13:10:39 -0600

    Use flags package for input args in randomfight

--- cmd/randomfight/randomfight.go
+++ cmd/randomfight/randomfight.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"love-letter-ai/gamemaster"
 	"love-letter-ai/players"
@@ -8,8 +9,12 @@ import (
 	"time"
 )
 
+var nTest = flag.Int("n", 1000, "Number of games played in each test against random")
+
 func main() {
+	flag.Parse()
 	rand.Seed(time.Now().UnixNano())
+
 	pls := []players.Player{
 		&players.RandomPlayer{},
 		&players.RandomPlayer{},
@@ -18,7 +23,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	winner, err := gm.PlaySeries(1000)
+	winner, err := gm.PlaySeries(*nTest)
 	if err != nil {
 		panic(err)
 	}
